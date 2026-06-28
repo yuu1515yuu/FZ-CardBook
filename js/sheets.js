6837ad2cc68e3d7367ce5187268d0a73e54ff92b
yuu1515yuu <yuu1515yuu@gmail.com>
2026-06-28 13:30:37 +0900

    Update sheets.js

--- js/sheets.js
+++ js/sheets.js
@@ -1,1 +1,47 @@
+/* ======================================================
+   FZ CardBook
+   config.js
+====================================================== */
 
+const CONFIG = {
+
+    app: {
+        name: "FZ CardBook",
+        version: "1.0.0"
+    },
+
+    spreadsheet: {
+
+        id: "1npvWira-vbWlPVP-uD1iFpMmfzRLv7bPP9d1DQ1ZYPI",
+
+        sheets: {
+
+            master: 0,
+
+            collection: 1545672613,
+
+            series: 719706617,
+
+            members: 389699866
+
+        }
+
+    },
+
+    members: {
+
+        karen: {
+            id: "karen",
+            name: "松本かれん",
+            color: "#FFB6D5"
+        },
+
+        yui: {
+            id: "yui",
+            name: "櫻井優衣",
+            color: "#98F0D0"
+        }
+
+    }
+
+};
