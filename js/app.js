ade4b01a3979af522881006d0f324b73da9ec120
yuu1515yuu <yuu1515yuu@gmail.com>
2026-06-28 12:54:17 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+/* ======================================================
+   FZ CardBook
+   app.js
+====================================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("FZ CardBook 起動");
+
+    document.getElementById("ownedRate").textContent = "0%";
+    document.getElementById("ownedCount").textContent = "0";
+    document.getElementById("missingCount").textContent = "0";
+
+});
