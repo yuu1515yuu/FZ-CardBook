7f8fed9dea4599ca60eb24d4cc8d5bb90475fb40
yuu1515yuu <yuu1515yuu@gmail.com>
2026-06-28 13:30:10 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,45 +10,37 @@ const CONFIG = {
         version: "1.0.0"
     },
 
+    spreadsheet: {
+
+        id: "1npvWira-vbWlPVP-uD1iFpMmfzRLv7bPP9d1DQ1ZYPI",
+
+        sheets: {
+
+            master: 0,
+
+            collection: 1545672613,
+
+            series: 719706617,
+
+            members: 389699866
+
+        }
+
+    },
+
     members: {
 
         karen: {
             id: "karen",
             name: "松本かれん",
-            color: "#FFB6D5",
-            icon: "🩷"
+            color: "#FFB6D5"
         },
 
         yui: {
             id: "yui",
             name: "櫻井優衣",
-            color: "#98F0D0",
-            icon: "💚"
+            color: "#98F0D0"
         }
-
-    },
-
-    cardTypes: {
-
-        paper: {
-            id: "paper",
-            name: "紙トレカ",
-            hasBackImage: true
-        },
-
-        clear: {
-            id: "clear",
-            name: "クリアトレカ",
-            hasBackImage: false
-        }
-
-    },
-
-    imagePath: {
-
-        base: "images/members/",
-
-        placeholder: "images/placeholder/no-image.png"
 
     }
 
